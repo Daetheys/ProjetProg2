cd6f12f20d04f8daf53241fb9e5f719f7f52bf9a
KisaraBlue <47254086+KisaraBlue@users.noreply.github.com>
2019-02-08 14:08:59 +0100

    Create level_generator.mli

---
+++ level_generator.mli
@@ -0,0 +1,7 @@
+Liste des objets/foncions nécessaires :
+
+- une classe 'tuiles principales' : obstacle ou couloir
+  - une sous-classe 'obstacle' : mur ou circuit ou mécanisme (couleur) ou grille
+  - une sous-classe 'couloir' : sol normal ou eau ou piques
+- une classe 'pièce' qui mappe une grille 15*21 à différents types de tuiles
+- une fonction
