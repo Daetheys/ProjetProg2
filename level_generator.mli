1842c4814f6f15640cdf020954dc2c0a82537669
KisaraBlue <47254086+KisaraBlue@users.noreply.github.com>
2019-02-08 17:21:50 +0100

    Update level_generator.mli

--- level_generator.mli
+++ level_generator.mli
@@ -4,4 +4,4 @@ Liste des objets/foncions nécessaires :
   - une sous-classe 'obstacle' : mur ou circuit ou mécanisme (couleur) ou grille
   - une sous-classe 'couloir' : sol normal ou eau ou piques
 - une classe 'pièce' qui mappe une grille 15*21 à différents types de tuiles
-- une fonction
+- une fonction qui donne un schéma de pièce aléatoire
