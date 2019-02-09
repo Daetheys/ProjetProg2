e57afa3d2df2d519dc5aa98c3754a6f57b9f35ca
KisaraBlue <47254086+KisaraBlue@users.noreply.github.com>
2019-02-09 12:55:58 +0100

    Update Mechanisms.scala

--- ProjetProg2/src/main/scala/Mechanisms.scala
+++ ProjetProg2/src/main/scala/Mechanisms.scala
@@ -2,4 +2,13 @@ Package Mechanisms
 // Pour la phase d'exploration
 import Schematics.{Obstacle=>Obstacle,_}
 
-class ObstacleType
+abstract class Tiletype
+  case class Circuit(c1:Int,c2:Int,dir:Boolean) extends Tiletype
+  case class Vault extends Tiletype
+  case class Pipe(c:Int,dir:Boolean) extends Tiletype
+  case class Plain
+
+class ObstacleType(d : String, t : Tiletype) extends Obstacle {
+  val description = d
+  val sprite = t
+}
