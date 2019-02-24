226d1ab6022485b45fbaa2815335484bb139d9be
KisaraBlue <47254086+KisaraBlue@users.noreply.github.com>
2019-02-24 17:07:11 +0100

    Update Mechanisms.scala

--- ProjetProg2/src/main/scala/Mechanisms.scala
+++ ProjetProg2/src/main/scala/Mechanisms.scala
@@ -15,11 +15,6 @@ abstract class Sprite_group
   case class Jail(anim : Int,
                   open : Boolean ) extends Sprite_group
 
-class ObstacleType(d : String, t : Tiletype) extends Obstacle {
-  val description = d
-  val sprite = t
-}
-
 
 
 class Sprite_grid
