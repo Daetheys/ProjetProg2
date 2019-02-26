e8443e07becfa15e3cd39890df79a29504a638e3
KisaraBlue <47254086+KisaraBlue@users.noreply.github.com>
2019-02-26 02:00:49 +0100

    Update Display.scala

--- ProjetProg2/src/main/scala/Display.scala
+++ ProjetProg2/src/main/scala/Display.scala
@@ -5,7 +5,7 @@ import Personnage.{Personnage=>Personnage,Jeton=>Jeton,_}
 
 class All_sprites(plan:Sprite_plan, jetons:List[Jeton]) {
   val main_grid = plan.all_the_sprites
-  def add_jetons():Array[Array[List[(String, Int]]] = {
+  def add_jetons():Array[Array[List[(String, Int)]]] = {
     var g = this.main_grid;
     for ( j <- jetons ) {
       g(j.x)(j.y) = g(j.x)(j.y) :+ (j.image_path, j.orientation);
