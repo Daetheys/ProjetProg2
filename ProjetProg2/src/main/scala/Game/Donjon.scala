b3991871c1e722928126d1ff9639252628a9eadb
Daetheys <daetheys@gmail.com>
2019-04-11 22:04:53 +0200

    .scala

---
+++ ProjetProg2/src/main/scala/Game/Donjon.scala
@@ -0,0 +1,81 @@
+package Donjon
+import Environnement._
+import Schematics._
+import Mechanisms._
+import Personnage._
+import Game._
+import Layer._
+import bddLevel._
+import Graphics2._
+import Display._
+
+class Donjon(taille:Int) {
+	val profondeur:Int = taille
+	val index_boss_stages:List[Int] = List(1)
+	// Attention boss_stages va etre remise a List() dans la suite
+	var boss_stages:List[(Array[Array[Int]],Array[Array[Option[Personnage]]],LayerSet)] = List(bddLevel.create_sentinel_level())
+	val stages:Array[Environnement] = Array.ofDim[Environnement](profondeur)
+	for (i<-0 to profondeur-1){
+		if (index_boss_stages.contains(i)){
+			val h = boss_stages.last
+			val tiles = h._1
+			var personnages = h._2
+			var layerset = h._3
+			val env = new Environnement
+			env.tiles = tiles.transpose
+			layerset.transpose()
+			env.layerset = layerset
+			this.personnages_spawn(env,personnages)
+			stages(i) = env
+			boss_stages = boss_stages.init
+		} else {
+			stages(i) = this.gen_level()
+		}
+	}
+	
+	var index_current_stage:Int = -1
+	
+	def start()={
+		if (index_current_stage == profondeur){
+			// WIN !!
+			app.win_screen()
+		} else {
+			val next_stage = this.get_next_stage()
+			//next_stage.load()
+			next_stage.start()
+		}
+	}
+	
+	def get_next_stage():Environnement={
+		this.index_current_stage += 1
+		return this.stages(index_current_stage)
+	}
+	
+	def personnages_spawn(Env:Environnement,personnages:Array[Array[Option[Personnage]]])={
+		//Spawn des unités sur l'environnement
+		for (i <- 0 to personnages.length-1){
+			for (j <- 0 to personnages(i).length-1){
+				Env.units(j)(i) = None
+				personnages(i)(j) match {
+					case None => ()
+					case Some(p:Personnage) => Env.spawn_personnage(p,j,i)
+				}
+			}
+		}
+	}
+	
+	def gen_level():Environnement={
+		//Chargement de l'objet contenant l'initialisation du terrain
+		val Env = new Environnement
+		val plan = new Plan
+		var sprite_plan = new Sprite_plan(plan);
+  		sprite_plan.random_loot();
+  		sprite_plan.fill_layer_0135();
+		val display = new All_sprites(sprite_plan)
+		Env.layerset = sprite_plan.everything
+		val personnages = display.personnages
+		this.personnages_spawn(Env,personnages)
+		Env.tiles = sprite_plan.tile_type
+		return Env
+	}
+}
---
+++ ProjetProg2/src/main/scala/Utilities/Utilities.scala
@@ -0,0 +1,86 @@
+package Utilities
+import Graphics2._
+import Personnage._
+
+//Helper functions
+object utils {
+	def cooldowned_fun(func:Unit=>Int,cd:Double,period:Period,base_arg:Int):Unit=>Int={ //Considere que func est appelée toutes les macro_period
+		//Monade pour ajouter un cooldown a des compétences -> déplacement plus lent // Attaque plus lente // ...
+		var base = base_arg
+		def cooldown (e:Unit):Int = {
+			//print(cd.toString+"\n")
+			//print(func.toString+"\n")
+			val compare = base*( period match { 
+				case Micro() => app.Env.clock.micro_period 
+				case Macro() => app.Env.clock.macro_period 
+				} )
+			if (compare >= cd) {
+				base = 1
+				return func(e)
+			}else{
+				base += 1
+				return 1
+			}
+		}
+		return cooldown
+	}
+	
+	def cooldowned(func:Unit=>Int,cd:Double):Unit=>Int={ //fresh initialement
+		this.cooldowned_fun(func,cd,Macro(),cd.toInt+1)
+	}
+	
+	def cooldowned_micro(func:Unit=>Int,cd:Double):Unit=>Int={
+		this.cooldowned_fun(func,cd,Micro(),cd.toInt+1)
+	}
+	
+	def delay(func:Unit=>Int,time:Double):Unit=>Int={ //Pas fresh initialement
+		this.cooldowned_fun(func,time,Macro(),1)
+	}
+	
+	def delay_micro(func:Unit=>Int,time:Double):Unit=>Int={
+		this.cooldowned_fun(func,time,Micro(),1)
+	}
+	
+	def chain(list_events:List[Unit=>Int]):Unit=>Int={ //-> Abus de prog fonctionelle
+		def exec_event(event:Unit=>Int):Unit=>Boolean={ //-> Boxage
+			var closed = false
+			def box(typage:Unit):Boolean={
+				if (closed) { return true } else { if (event() == 1) {return false } else {closed = true;return true}}
+			}
+			return box
+		}
+		def fold_fun(event:Unit=>Int,new_event:Unit=>Int):Unit=>Int={ //-> Recursivité complexe (pas hyper opti mais on ne lancera pas chain sur des listes trop grandes
+			var closed = false
+			def fun(typage:Unit):Int={
+				if (closed || new_event() == 0){ //-> Surapplication
+								closed = true
+								return event()
+				} else { 
+								return 1
+				}
+			}
+			return fun
+		}
+		def init_fun(typage:Unit):Int={
+			return 0
+		}
+		return list_events.reverse.fold(init_fun(_))(fold_fun(_,_))
+	}
+	
+	def dummy_event(typage:Unit):Int={
+		return 0
+	}
+	
+	def shoot(x1:Int,y1:Int,target:Jeton,dmg:Int)={
+		def delayed(typage:Unit):Int={
+			target.model.take_damages(dmg)
+			return 0
+		}
+		app.draw_shoot_line(x1,y1,target.x,target.y)
+		app.Env.clock.add_micro_event(delay_micro(delayed(_),20*target.Env.clock.micro_period))
+	}
+}
+
+abstract class Period
+case class Micro() extends Period
+case class Macro() extends Period
